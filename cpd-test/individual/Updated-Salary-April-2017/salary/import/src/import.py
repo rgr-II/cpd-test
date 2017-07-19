7a5b07bbf75a00d30d0aff3acfac89ad44065920
Roman Rivera <roman@invisibleinstitute.com>
2017-07-19 14:19:38 -0500

    import cleaned

--- cpd-test/individual/Updated-Salary-April-2017/salary/import/src/import.py
+++ cpd-test/individual/Updated-Salary-April-2017/salary/import/src/import.py
@@ -9,6 +9,6 @@ for f in files:
     xls = pd.ExcelFile(f)
     for s in xls.sheet_names:
         df = xls.parse(s)
-        df.columns = ["Last_Name", "First_Name", "Middle_Initial", "Gender", "Age_at_Hire", "Title", "Start_Date", "SPP", "Org_Hire_Date", "Salary", "Pay_Grade", "Employee_Status"]
-        df.to_csv("salary-" + s + ".csv", index=False)
-        metadata_dataset(df ,f + "-" + s, "salary-"+s+".csv").to_csv('metadata_' + 'salary-' + s + '.csv', index=False)
+        df.columns = CorrectColumns(df.columns)
+        df.to_csv("../output/salary-" + s + ".csv", index=False)
+        metadata_dataset(df ,f + "-" + s, "salary-"+s+".csv").to_csv('../output/metadata_' + 'salary-' + s + '.csv', index=False)
